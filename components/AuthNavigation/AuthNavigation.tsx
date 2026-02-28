7a088e12860f0b70641bfa0c8dfb4bef80ef1f12
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-28 17:24:51 +0200

    Update navigation

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -1,7 +1,11 @@
-import Link from 'next/link';
-import css from './AuthNavigation.module.css'
+"use client";
+
+import Link from "next/link";
+import css from "./AuthNavigation.module.css";
+import { useAuthStore } from "@/lib/store/authStore";
 
 export default function AuthNavigation() {
+    const { user, isAuthenticated, clearIsAuthenticated } = useAuthStore();
     return (
         <>
             <li className={css.navigationItem}>
@@ -9,23 +13,37 @@ export default function AuthNavigation() {
                     Profile
                 </Link>
             </li>
+            {isAuthenticated ? (
+                <li className={css.navigationItem}>
+                    <p className={css.userEmail}>User email</p>
+                    <button className={css.logoutButton} suppressHydrationWarning>
+                        Logout
+                    </button>
+                </li>
+            ) : (
+                <>
+                    <li className={css.navigationItem}>
+                        <Link
+                            href="/sign-in"
+                            className={css.navigationLink}
+                            prefetch={false}
+                            suppressHydrationWarning
+                        >
+                            Login
+                        </Link>
+                    </li>
 
-            <li className={css.navigationItem}>
-                <p className={css.userEmail}>User email</p>
-                <button className={css.logoutButton} suppressHydrationWarning>Logout</button>
-            </li>
-
-            <li className={css.navigationItem}>
-                <Link href="/sign-in" className={css.navigationLink} prefetch={false} suppressHydrationWarning>
-                    Login
-                </Link>
-            </li>
-
-            <li className={css.navigationItem}>
-                <Link href="/sign-up" className={css.navigationLink} prefetch={false}>
-                    Sign up
-                </Link>
-            </li>
+                    <li className={css.navigationItem}>
+                        <Link
+                            href="/sign-up"
+                            className={css.navigationLink}
+                            prefetch={false}
+                        >
+                            Sign up
+                        </Link>
+                    </li>
+                </>
+            )}
         </>
     );
 }
