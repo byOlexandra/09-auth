db2928ad7d38db1c490c3b4762153795bfc905b8
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-02-28 21:47:58 +0200

    Fix imports

--- app/(private routes)/notes/[id]/page.tsx
+++ app/(private routes)/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import NotePreview from "@/app/@modal/(.)notes/[id]/NotePreview.client";
-import { fetchNoteById } from "@/lib/clientApi";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import {
     dehydrate,
     HydrationBoundary,
