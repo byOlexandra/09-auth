9d6649897f049b882da34d4c443f930abe3cdeee
Староватова Олександра <ist251_sov@student.ztu.edu.ua>
2026-03-01 10:04:48 +0200

    Fix metadata

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,24 +3,31 @@ import { getMe } from "@/lib/api/serverApi";
 import ProfilePage from "./ProfilePage";
 
 export async function generateMetadata(): Promise<Metadata> {
-    const user = await getMe();
+    try {
+        const user = await getMe();
 
-    return {
-        title: `${user?.username.charAt(0).toUpperCase() + user?.username.slice(1) || 'User'} profile`,
-        description: `${user?.email || 'User'}`,
-        openGraph: {
-            title: `${user?.username.charAt(0).toUpperCase() || 'User'} profile`,
+        return {
+            title: `${user?.username.charAt(0).toUpperCase() + user?.username.slice(1) || 'User'} profile`,
             description: `${user?.email || 'User'}`,
-            url: `/profile`,
-            images: [
-                {
-                    url: `${user?.avatar}`,
-                    width: 1200,
-                    height: 630,
-                    alt: `Note: ${user?.username.charAt(0).toUpperCase() || 'User'} profile`
-                }
-            ]
+            openGraph: {
+                title: `${user?.username.charAt(0).toUpperCase() || 'User'} profile`,
+                description: `${user?.email || 'User'}`,
+                url: `/profile`,
+                images: [
+                    {
+                        url: `${user?.avatar}`,
+                        width: 1200,
+                        height: 630,
+                        alt: `Note: ${user?.username.charAt(0).toUpperCase() || 'User'} profile`
+                    }
+                ]
+            }
         }
+    } catch (error) {
+        return {
+            title: "User Profile",
+            description: "View your personal profile on NoteHub"
+        };
     }
 }
 
